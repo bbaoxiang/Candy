d62dd6354ca84e97e837efef123774f4a042c1ac
baoxi <baoxi@DESKTOP-LVE1033>
2021-10-10 20:53:54 +0800

    添加项目文件。

---
+++ goods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Candy
+{
+    public class goods
+    {
+        public string asin { set; get; }
+        public string color { set; get; }
+        public string size { set; get; }
+        public string price { set; get; }
+        public string shop { set; get; }
+    }
+}
